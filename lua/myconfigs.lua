e2c3beaaa2887f882351ca3dca7c5fba701badbc
sadigu <menis@intentiq.com>
2024-11-25 11:58:20 +0200

    a better statuscolumn

--- lua/myconfigs.lua
+++ lua/myconfigs.lua
@@ -1,7 +1,6 @@
+vim.opt.statuscolumn = '%=%s%C%{v:relnum?v:relnum:v:lnum} '
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
-vim.opt.signcolumn = 'number'
-vim.opt.foldcolumn = '0'
 
 -- Help nvim find python so it won't take it extra second to find it by it self
 vim.g.python3_host_prog = '/Users/meni/.pyenv/versions/nvim/bin/python3'
