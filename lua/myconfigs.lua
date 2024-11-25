6ce3d37f8003f0a30c361ea392753b938d923321
sadigu <menis@intentiq.com>
2024-11-25 10:22:05 +0200

    added relative number lines

--- lua/myconfigs.lua
+++ lua/myconfigs.lua
@@ -1,2 +1,6 @@
+vim.opt.statuscolumn = '%=%{v:relnum?v:relnum:v:lnum} '
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+
 -- Help nvim find python so it won't take it extra second to find it by it self
 vim.g.python3_host_prog = '/Users/meni/.pyenv/versions/nvim/bin/python3'
