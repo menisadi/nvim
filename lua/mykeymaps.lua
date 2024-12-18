b8a8edddc9934b2401b93a50d8403d13dd4f458c
menisadi <menisadi@gmail.com>
2024-12-18 10:45:33 +0200

    change completion select to tab

--- lua/mykeymaps.lua
+++ lua/mykeymaps.lua
@@ -1,3 +1,8 @@
 vim.keymap.set('n', ']b', ':bnext<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '[b', ':bprev<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>co', '<cmd>AerialToggle<CR>')
+
+-- vim.keymap.set('i', '<Tab>', function()
+--   return vim.fn.pumvisible() == 1 and '<C-y>' or '<Tab>'
+-- end, { expr = true })
+-- vim.keymap.set('i', '<Tab>', '<C-y>', { expr = true, noremap = true, silent = true })
