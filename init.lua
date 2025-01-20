d9c82c4ce1259ca186d3fd932f977f4c75e053a4
menisadi <menisadi@gmail.com>
2025-01-20 18:06:13 +0200

    removed keychain for now
    will add as a proper plugin later

--- init.lua
+++ init.lua
@@ -828,7 +828,6 @@ require('lazy').setup({
 require 'mykeymaps'
 require 'myautocmds'
 require 'myconfigs'
-require('keychain').setup()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
