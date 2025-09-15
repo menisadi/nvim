957fb8b71d7b64410e2bcfa872189b6b5613f235
menisadi <menisadi@gmail.com>
2025-09-15 09:11:02 +0300

    Better Hebrew Mode

--- lua/myfunctions.lua
+++ lua/myfunctions.lua
@@ -1,25 +1,59 @@
-function ToggleHebrewMode()
+-- steady block for Normal/Elsewhere
+-- steady bar for LTR insert
+-- steady underline for RTL insert
+local guicursor_ltr = table.concat({
+  'n-v-c:block', -- normal/visual/cmdline: block
+  'i-ci-ve:ver25', -- insert/insert-cmd/visual-exclude: vertical bar (steady)
+  'r-cr:hor20', -- replace modes: underline
+  'o:hor50', -- operator-pending
+}, ',')
+
+local guicursor_rtl = table.concat({
+  'n-v-c:block', -- normal/visual/cmdline: block
+  'i-ci-ve:hor20', -- insert becomes underline (steady) for RTL
+  'r-cr:hor20',
+  'o:hor50',
+}, ',')
+
+-- Default to LTR cursor shapes
+vim.o.guicursor = guicursor_ltr
+
+local function ToggleHebrewMode()
   local heb_mode = vim.b.hebrew_mode_enabled
 
-  if heb_mode == nil or heb_mode == false then
-    -- Turn Hebrew mode ON
-    vim.opt_local.keymap = 'hebrew' -- Use 'hebrew' keymap for insert-mode
-    vim.opt_local.iminsert = 1 -- Make sure insert mode uses the 'keymap'
-    vim.opt_local.imsearch = 1 -- Same for search
-    vim.opt_local.rightleft = true -- Enable right-to-left mode for this window
+  if not heb_mode then
+    -- --- Turn Hebrew mode ON (RTL) ---
+    vim.opt_local.keymap = 'hebrew'
+    vim.opt_local.iminsert = 1
+    vim.opt_local.imsearch = 1
+    vim.opt_local.rightleft = true
     vim.b.hebrew_mode_enabled = true
+
+    -- Switch insert cursor to steady underline
+    vim.o.guicursor = guicursor_rtl
+
     vim.notify('Hebrew mode: ON', vim.log.levels.INFO)
   else
-    -- Turn Hebrew mode OFF
-    vim.opt_local.keymap = '' -- Clear the insert-mode keymap
+    -- --- Turn Hebrew mode OFF (LTR) ---
+    vim.opt_local.keymap = ''
     vim.opt_local.iminsert = 0
     vim.opt_local.imsearch = 0
     vim.opt_local.rightleft = false
     vim.b.hebrew_mode_enabled = false
+
+    -- Switch insert cursor back to steady bar
+    vim.o.guicursor = guicursor_ltr
+
     vim.notify('Hebrew mode: OFF', vim.log.levels.INFO)
   end
 end
 
+-- Expose as a user command
 vim.api.nvim_create_user_command('HebrewToggle', function()
   ToggleHebrewMode()
 end, {})
+
+-- Keymap: <leader>h to toggle
+vim.keymap.set('n', '<leader>hb', function()
+  vim.cmd.HebrewToggle()
+end, { silent = true, desc = 'Toggle Hebrew mode (RTL/LTR)' })
