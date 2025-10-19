ed3dc207ae028493ff1d63f4d005dcb506043a36
menisadi <menisadi@gmail.com>
2025-10-19 10:00:23 +0300

    Separation - > and <

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -4,8 +4,6 @@ return {
     options = {
       icons_enabled = true,
       theme = 'kanagawa',
-      component_separators = '|',
-      section_separators = '',
       globalstatus = true,
     },
     tabline = {
