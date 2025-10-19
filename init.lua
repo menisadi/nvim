2c6b0a0f9c2073e2317e8e2a419bd44825734798
menisadi <menisadi@gmail.com>
2025-10-19 21:46:01 +0300

    Removing the colorscheme from ini.lua

--- init.lua
+++ init.lua
@@ -741,17 +741,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'rebelot/kanagawa.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      vim.cmd.colorscheme 'kanagawa'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   {
