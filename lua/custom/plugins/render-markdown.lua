0c40a1cc7948c7ae3e841d7c6bf54d6202b872a0
menisadi <menisadi@gmail.com>
2025-10-22 21:30:10 +0300

    Lazy loading render-markdown
    Plus a hotkey for it

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -1,7 +1,12 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
+  cmd = 'RenderMarkdown',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
+  keys = {
+    { '<leader>rm', '<cmd>RenderMarkdown<cr>', desc = 'Render Markdown' },
+    { '<leader>rd', '<cmd>RenderMarkdown disable<cr>', desc = 'Disable Render Markdown' },
+  },
 }
