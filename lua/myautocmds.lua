612856e1c2abe9ee55be9fb685f82ed084241c09
menisadi <menisadi@gmail.com>
2026-02-24 09:27:55 +0200

    Added light/dark auto switch
    Still need to  modify the tab bar and status bar

--- lua/myautocmds.lua
+++ lua/myautocmds.lua
@@ -1,7 +1,10 @@
--- Simple example of creating an autocommand in Neovim using Lua
--- vim.api.nvim_create_autocmd('BufWritePost', {
---   pattern = '*',
---   callback = function()
---     print 'File saved! 🥳'
---   end,
--- })
+vim.api.nvim_create_autocmd('OptionSet', {
+  pattern = 'background',
+  callback = function()
+    if vim.o.background == 'dark' then
+      vim.cmd.colorscheme 'kanagawa'
+    else
+      vim.cmd.colorscheme 'default'
+    end
+  end,
+})
