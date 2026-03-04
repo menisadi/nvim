ff9426c02c9988abb7fb93155ff960cb1cca65bf
sadigu <menis@intentiq.com>
2026-03-04 13:37:29 +0200

    Added blink and fzf-lua

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,8 @@
+return {
+	"saghen/blink.cmp",
+	version = "*",
+	opts = {
+		keymap = { preset = "default" },
+		signature = { enabled = true },
+	},
+}
---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,10 @@
+return {
+	"ibhagwan/fzf-lua",
+	opts = {},
+	keys = {
+		{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files" },
+		{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Live Grep" },
+		{ "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Buffers" },
+		{ "<leader>fh", "<cmd>FzfLua helptags<cr>", desc = "Help Tags" },
+	},
+}
