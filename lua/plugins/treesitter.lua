fbb26411dfcdb0345f9d6bf1bc2109abd05c25a2
sadigu <menis@intentiq.com>
2026-03-04 13:46:08 +0200

    Removing parats we don't need for now

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
 	opts = {
 		ensure_installed = {
 			"bash",
-			"c",
 			"diff",
-			"html",
 			"lua",
 			"luadoc",
 			"markdown",
@@ -15,12 +12,8 @@ return {
 			"query",
 			"vim",
 			"vimdoc",
+			"python",
 		},
 		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
 	},
 }
