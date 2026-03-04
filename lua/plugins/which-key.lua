7d1465d3daa213a03b92adf898c169b2415f1628
sadigu <menis@intentiq.com>
2026-03-04 20:52:19 +0200

    Removed leftovers from which key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,19 +3,14 @@ return {
 	event = "VimEnter",
 	opts = {
 		preset = "helix",
-		icons = {
-			mappings = vim.g.have_nerd_font,
-		},
 		spec = {
 			{ "<leader>l", group = "[L]sp", mode = { "n", "x" } },
 			{ "<leader>d", group = "[D]ocument" },
 			{ "<leader>f", group = "[F]zf" },
-			{ "<leader>r", group = "[R]ender" },
-			{ "<leader>s", group = "[S]earch" },
 			{ "<leader>w", group = "[W]orkspace" },
 			{ "<leader>t", group = "[T]oggle" },
 			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 			{ "<leader>x", group = "Trouble" },
 		},
 	},
-}
+
