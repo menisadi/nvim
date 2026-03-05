a96b253df52e6c04a4bd261fff5552df34e71bfd
sadigu <menis@intentiq.com>
2026-03-05 14:12:45 +0200

    Added proper icon
    Without underlines (which is the defualt hl group)

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,10 +4,10 @@ return {
 	opts = {
 		preset = "helix",
 		spec = {
-			{ "<leader>l", group = "[L]sp", icon = "", mode = { "n", "x" } },
-			{ "<leader>f", group = "[F]zf", icon = "", mode = { "n", "v" } },
-			{ "<leader>g", group = "[G]it", icon = "", mode = { "n", "v" } },
-			{ "<leader>O", desc = "Open Oil", icon = "", mode = { "n", "v" } },
+			{ "<leader>l", group = "[L]sp", icon = { icon = "󰒕", hl = "Keyword" }, mode = { "n", "x" } },
+			{ "<leader>f", group = "[F]zf", icon = { icon = "󰍉", hl = "Keyword" }, mode = { "n", "v" } },
+			{ "<leader>g", group = "[G]it", icon = { icon = "󰊢", hl = "Keyword" }, mode = { "n", "v" } },
+			{ "<leader>O", desc = "Open Oil", icon = { icon = "󰉋", hl = "Keyword" }, mode = { "n", "v" } },
 		},
 	},
 }
