47edeb68f2d3a76f6e1a1240b2f603a4c0b48272
sadigu <menis@intentiq.com>
2026-03-20 01:36:10 +0200

    fix: TS are now working properly

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,23 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"bash",
-			"diff",
-			"lua",
-			"luadoc",
-			"markdown",
-			"markdown_inline",
-			"query",
-			"vim",
-			"vimdoc",
-			"python",
-		},
-		auto_install = true,
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"gleam",
+				"bash",
+				"diff",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+				"python",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+		})
+	end,
 }
